33b2c153521b62f14225fde9e5a2b388ab595900
Zagalskiy <izagalskiy@mail.ru>
2023-11-19 06:51:10 +0300

    ML model with parameters

--- scripts/production/app.py
+++ scripts/production/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return("Hello from Flask!")
+
+@app.route("/predict/<int:age>/<int:education_num>/<int:sex>/<int:capital_gain>")
+def predict(age, education_num, sex, capital_gain):
+    with open("./models/model.pkl", "rb") as fd:
+        clf = pickle.load(fd)
